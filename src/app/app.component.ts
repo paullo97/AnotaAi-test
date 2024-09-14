54967d5a56b524e731c74f0c454ec4deab153cea
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-09-14 01:23:04 -0300

    import and include in template component to search, include some scss to define body app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { SearchInputComponent } from "./components/search-input/search-input.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, SearchInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
