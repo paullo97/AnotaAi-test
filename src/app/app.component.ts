a028d21e3d758962be9f2a7c3ccf76892a8f500c
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-09-14 02:08:44 -0300

    reference card in template and implements new class to control list cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { SearchInputComponent } from "./components/search-input/search-input.component";
+import { CardComponent } from "./components/card/card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SearchInputComponent],
+  imports: [RouterOutlet, HeaderComponent, SearchInputComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
