a4c69c72a06ed3c302b81e06d58466cda1186b3b
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-09-14 03:44:36 -0300

    include some function to search, not work yet

--- src/app/components/search-input/search-input.component.ts
+++ src/app/components/search-input/search-input.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search-input.component.html',
   styleUrl: './search-input.component.scss'
 })
 export class SearchInputComponent {
 
+  @Output()
+  public search: EventEmitter<string> = new EventEmitter<string>();
+
+  public text: string = '';
+
+  public Search(word: string): void
+  {
+    this.search.emit(word);
+  }
 }
