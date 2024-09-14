c8aa210fd8322339485a6dfa75dd2da685ce85b9
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-09-14 03:46:17 -0300

    implement some function to request data and prepare to implement others functions - WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { SearchInputComponent } from "./components/search-input/search-input.component";
 import { CardComponent } from "./components/card/card.component";
+import { ServicesModule } from './core/services/services.module';
+import { DataService } from './core/services/data.service';
+import { StorageService } from './core/services/storage.service';
+import { IItem } from './core/model/item.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SearchInputComponent, CardComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    SearchInputComponent,
+    CardComponent,
+    ServicesModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'AnotaAi-test';
+export class AppComponent implements OnInit {
+
+  public loading: boolean = true;
+  public data: Array<IItem> = [];
+
+  constructor(
+    private readonly dataService: DataService,
+    private readonly storage: StorageService
+  )
+  { }
+
+  public ngOnInit(): void {
+    this.dataService.getData()
+      .then((response: Array<IItem>) => {
+        this.data = response;
+        this.storage.setItem('data', this.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data', error);
+      })
+      .finally(() => {
+        this.loading = false;
+      });
+  }
+
+  public searchItem(param: string): void
+  {
+    console.log(param);
+  }
+
+  public deleteItem(item: IItem): void {
+    console.log(item);
+  }
 }
