be69d045fc84cb7022975ac240dc2f3e190b48ab
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-09-14 03:43:59 -0300

    implements some funcions and include data bind to show info

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
+import { IItem } from '../../core/model/item.model';
+import { EnumType } from '../../core/model/type.model';
 
 @Component({
   selector: 'app-card',
@@ -9,4 +11,29 @@ import { Component } from '@angular/core';
 })
 export class CardComponent {
 
+  @Input()
+  public item: IItem | undefined;
+
+  @Output()
+  public deleteItem: EventEmitter<IItem> = new EventEmitter<IItem>();
+
+  public defineType(type: number): string {
+    return {
+      1: EnumType.Paisagem,
+      2: EnumType.Flor,
+      3: EnumType.Pizza
+    }[type] || EnumType.Paisagem
+  }
+
+  public defineClassChip(type: number): string {
+    return {
+      1: 'paisagem',
+      2: 'flor',
+      3: 'pizza'
+    }[type] || 'paisagem'
+  }
+
+  public handleDeleteItem(): void {
+    this.deleteItem.emit(this.item);
+  }
 }
