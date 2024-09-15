f8d0683ed981e172e8b675b36813744d0512cc65
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-09-14 22:55:48 -0300

    create component for no result in system

---
+++ src/app/components/no-result/no-result.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-result',
+  standalone: true,
+  imports: [],
+  templateUrl: './no-result.component.html',
+  styleUrl: './no-result.component.scss'
+})
+export class NoResultComponent {
+
+}
