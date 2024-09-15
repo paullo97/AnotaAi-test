44c1de70d06f00c9b04eb76a0e4ecebcfdd26a94
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-09-14 22:06:36 -0300

    Create service for toast in system

---
+++ src/app/components/toast/toast.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toast',
+  standalone: true,
+  template: `<div id="toast"></div>`,
+  styleUrls: ['./toast.component.scss']
+})
+export class ToastComponent {}
---
+++ src/app/core/services/toast.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastService {
+  constructor() {}
+
+  showToast({ title = "", message = "", type = "info", duration = 3000 }: { title: string, message: string, type: string, duration: number }) {
+    const toastContainer = document.getElementById('toast');
+    if (toastContainer) {
+      const toast = document.createElement('div');
+
+      const autoRemoveId = setTimeout(() => {
+        toastContainer.removeChild(toast);
+      }, duration + 1000);
+
+      toast.onclick = (event) => {
+        const target = event.target as HTMLElement;
+        if (target.closest('.toast__close')) {
+          toastContainer.removeChild(toast);
+          clearTimeout(autoRemoveId);
+        }
+      };
+
+      const delay = (duration / 1000).toFixed(2);
+
+      toast.classList.add('toast', `toast--${type}`);
+      toast.style.animation = `slideInLeft ease .3s, fadeOut linear 1s ${delay}s forwards`;
+
+      toast.innerHTML = `
+        <div class="toast__icon">
+        </div>
+        <div class="toast__body">
+          <h3 class="toast__title">${title}</h3>
+          <p class="toast__msg">${message}</p>
+        </div>
+        <div class="toast__close">
+          <i>X</i>
+        </div>
+      `;
+
+      toastContainer.appendChild(toast);
+    }
+  }
+}
